a7fbf418918eb96be874530e15c4726e8360cb91
Will Saulnier <wsaulnier1004@gmail.com>
2025-06-04 16:47:25 -0400

    messing with graph

--- components/Graph.js
+++ components/Graph.js
@@ -1,9 +1,10 @@
 // Graph.js
 
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState, useRef } from 'react';
 import { View, StyleSheet, Dimensions, Text } from 'react-native';
 import { LineChart } from 'react-native-chart-kit';
 import { DataContext } from '../contexts/DataContext';
+import { SelectList } from 'react-native-dropdown-select-list';
 
 const screenWidth = Dimensions.get('window').width;
 
@@ -20,12 +21,60 @@ const chartConfig = {
 };
 
 const Graph = () => {
-  const { dataPoints, elapsed } = useContext(DataContext);
+  const { adcValue, elapsed } = useContext(DataContext);
+  // console.log("graph render", dataPoints.length)
+  const  [selected, setSelected] = useState("")
+  const  [dataPoints, setDataPoints] = useState([])
+  const  [elapsedPoints, setElapsedPoints] = useState([])
+
+  const counterRef = useRef(0)
+  
+
+  useEffect(() => {
+    const freq = parseInt(selected) || 1; // Default to 1 if not selected
+
+
+    if (
+      adcValue !== undefined &&
+      adcValue !== null &&
+      elapsed !== undefined &&
+      elapsed !== null
+    ) {
+      if (counterRef.current === 0) {
+        setDataPoints(prev => [...prev, adcValue].slice(-100));
+        setElapsedPoints(prev => [...prev, elapsed].slice(-100));
+      }
+      counterRef.current = (counterRef.current + 1) % freq;
+    }
+  }, [adcValue]);
+
+
+  let labels = Array(dataPoints.length).fill("");
+if (elapsedPoints.length > 0) {
+  const n = elapsedPoints.length;
+  labels[0] = (elapsedPoints[0] / 1000).toFixed(1);
+  labels[Math.floor(n / 2)] = (elapsedPoints[Math.floor(n / 2)] / 1000).toFixed(1);
+  labels[n - 5] = (elapsedPoints[n - 1] / 1000).toFixed(1);
+}
+
+
+  
+  const data = [
+    {key:'1', value:'1'},
+    {key:'2', value:'5'},
+    {key:'3', value:'10'}
+  ]
 
   return (
     <View style={styles.graphContainer}>
+      <SelectList 
+        setSelected={(val) => {setSelected(val)}}
+        data = {data}
+        save = "value"
+      />
       <LineChart
         data={{
+          labels:labels,
           datasets: [
             {
               data: dataPoints,
@@ -48,7 +97,6 @@ const Graph = () => {
         withVerticalLines={false}
 
       />
-      <Text>{ elapsed }</Text>
     </View>
   );
 };
@@ -67,7 +115,7 @@ const styles = StyleSheet.create({
     width: screenWidth - 32, // Match the width prop of LineChart
     borderRadius: 10,
     alignSelf: 'center',     // Center the chart within the container
-    marginLeft: -50
+    marginLeft: -50         // adjust left for invisible y axis labels 
   },
 });
 
